d5d0fad9f0833a798aabe77c2dd5f5476e196075
rt134 <rishabh.tripathi134@gmail.com>
2022-07-04 09:06:19 +0530

    commit02

--- 24-Azure-AKS-Terraform/24-05-Create-AKS-Cluster-Custom-VNET/terraform-manifests-aks-custom-vnet/07-aks-cluster.tf
+++ 24-Azure-AKS-Terraform/24-05-Create-AKS-Cluster-Custom-VNET/terraform-manifests-aks-custom-vnet/07-aks-cluster.tf
@@ -81,10 +81,10 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   }
 
 # Windows Profile
-  # windows_profile {
-  #   admin_username = var.windows_admin_username
-  #   admin_password = var.windows_admin_password
-  # }
+  windows_profile {
+    admin_username = var.windows_admin_username
+    admin_password = var.windows_admin_password
+  }
 
 # Linux Profile
   linux_profile {
